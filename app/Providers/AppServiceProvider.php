0dda6dd97901e123a4ae4db3a3825dce6f256987
atmakarimang <atmakarimang@gmail.com>
2021-11-11 16:46:53 +0700

    update otorisasi admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Paginator::useBootstrap();
+
+        Gate::define('admin', function (User $user) {
+            return $user->user_role === 'admin';
+        });
     }
 }
